61b94ba3d25d8a0b28bfb97bf95220e2c4d9d164
Honeybar <verina_jean@hotmail.com>
2018-06-26 11:37:58 -0700

    Add files via upload

--- GCMaxTemp.py
+++ GCMaxTemp.py
@@ -4,59 +4,77 @@ import pandas as panda
 
 # ML Algo
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
+import numpy as np
+from numpy.polynomial.polynomial import polyfit
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+
+def getGenus(traindata, genus):
+    traindata["Genus"] = traindata["GenusSTR"].map(genus)
+    traindata = traindata.drop(["GenusSTR"], 1)
+    return traindata
+
+def convertTemp(traindata): # convert temperature data into integer and average the range temperature
+    traindata["Temp"] = traindata["Temp"].map(lambda x: str(x))
+    traindata['Temp'] = traindata["Temp"].map(lambda x: sum(map(int, (str(x).split("-")))) / len(str(x).split("-")))
+    return traindata
 
 #get the training and test data from file
-train_df = panda.read_csv("MLTraining.csv")
-test_df = panda.read_csv("MLTest.csv")
+train_df = panda.read_csv("Bacteria_list.csv")
+test_df = panda.read_csv("Bacteria_test.csv")
 
-#Seting up training and test data using G-C Content to predict optimum temperature
-X_train = train_df["GC Content (%)"]
-X_train = X_train.reshape(len(X_train), 1)
-Y_train = train_df["Max Temp (C)"]
-Y_train = Y_train.reshape(len(Y_train), 1)
+# get all types of genus
+genus = {}
+train_df["GenusSTR"] = train_df["Bacteria"].str.split(" ").str[0]
+gencode = 1  # code for mapping each genus to a number for random forest
 
-#print len(X_train) == len(Y_train)
-X_test = test_df["GC Content (%)"]
-X_test = X_test.reshape(len(X_test), 1)
-Y_test = test_df["Max Temp (C)"]
-Y_test = Y_test.reshape(len(Y_test), 1)
+for index, row in train_df.iterrows():
+    if row["GenusSTR"] not in genus:  # find all the available genus in the training data
+        genus[row["GenusSTR"]] = gencode
+        gencode += 1
 
-#Training data using LinearRegression
-linear = LinearRegression()
-linear.fit(X_train, Y_train)
-print "accuracy score for linear regression (GCC vs temperature): ", linear.score(X_test, Y_test) #Ans: -0.406938582357
+train_df = getGenus(train_df, genus) # convert all genus to a numerical data
+train_df = convertTemp(train_df) # convert temperature data into integer
 
-#So genus matters based on the result above.
+test_df["GenusSTR"] = test_df["Bacteria"].str.split(" ").str[0]
+test_df = getGenus(test_df, genus)
+test_df = convertTemp(test_df)
 
-genus = {"Photorhabdus":1, "Shewanella":2, "Aliivibrio":3, "Photobacterium":4, "Vibrio": 5}
-
-# get all types of genus and convert them to numeric data
-train_df["GenusSTR"] = train_df["Organism"].str.split(" ").str[0]
-train_df["Genus"] = train_df["GenusSTR"].map(genus)
-train_df = train_df.drop(["GenusSTR"], 1)
-
-test_df["GenusSTR"] = test_df["Organism"].str.split(" ").str[0]
-test_df["Genus"] = test_df["GenusSTR"].map(genus)
-test_df = test_df.drop(["GenusSTR"], 1)
 
 #setting up training and test data using features like G-C content and genus to predict optimum temperature
-X_train = train_df.drop(train_df.columns[[0, 1]], 1)
-Y_train = train_df["Max Temp (C)"]
+X_train = train_df[["Temp", "Genus"]]
+Y_train = train_df["GC Content"]
 
-X_test = test_df.drop(test_df.columns[[0, 1]], 1)
-Y_test = test_df["Max Temp (C)"]
+X_test = test_df[["Temp", "Genus"]]
+Y_test = test_df["GC Content"]
 
 #Training data using RandomForest
 random_forest = RandomForestRegressor()
 random_forest.fit(X_train, Y_train)
+print "accuracy score for Random Forest Regressor (temperature, genus vs GC content): ", random_forest.score(X_test, Y_test)
 
-print "accuracy score for Random Forest Regressor (GCC, genus vs temperature): ", random_forest.score(X_test, Y_test)
-
-# FInd out the importance of each feature
+# Find out the importance of each feature
 importances = panda.DataFrame({'feature':X_train.columns,'importance':np.round(random_forest.feature_importances_,3)})
-importances = importances.sort_values('importance',ascending=False).set_index('feature')
-print importances.head(15)
+#plt.figure()
+#plt.title("Feature importances")
+#plt.bar(np.arange(len(importances["feature"])), importances["importance"], align="center")
+#plt.xticks(np.arange(len(importances["feature"])), importances["feature"])
+#importances = importances.sort_values('importance',ascending=False).set_index('feature')
+print importances.head(15), "\n"
+
+
+#Checking correlation between GC content and temperature
+#print "correlation between GC content and temperature: ", train_df.corr()
+print "correlation between GC content and temperature: ", pearsonr(train_df["Temp"], train_df["GC Content"])
+print "number of genus: ", len(genus)
+
+plt.plot(train_df["Temp"], train_df["GC Content"], ".")
+plt.plot(train_df["Temp"], np.poly1d(np.polyfit(train_df["Temp"], train_df["GC Content"], 1))(train_df["Temp"]))
+plt.ylabel("GC content")
+plt.xlabel("Temperature (C)")
+plt.figure()
+plt.show()
 
 
 
